3f1175e5d2c90c23ea741392c54772ba48f11954
Dyana Varghese <dyna1.arackal@gmail.com>
2022-12-07 22:43:45 +0000

    Initial Commit

---
+++ GoAroundNewsApp/GoAroundNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoAroundNewsAppApp.swift
+//  GoAroundNewsApp
+//
+//  Created by Dyana Varghese on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GoAroundNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
